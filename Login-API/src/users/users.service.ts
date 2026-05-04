5060786a9d4c336f6f135fdea73fdc658d22c15f
luan menezesds <luanmenezesds615@gmail.com>
2026-05-04 19:46:33 -0300

    chore: initial nestjs structure and auth dependencies

---
+++ Login-API/src/users/users.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+
+export type User = any;
+@Injectable()
+export class UsersService {
+  private readonly users = [
+    {
+      userId: 1,
+      username: 'luan',
+      password: 'senha_criptografada_aqui',
+    },
+  ];
+
+  async findOne(username: string): Promise<User | undefined> {
+    return this.users.find(user => user.username === username);
+  }
+}
